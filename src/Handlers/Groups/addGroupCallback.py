5b3ae335dc2e8696f50ff6de27d8614b24d6aef4
Fanboy041 <hanialhusen041@gmail.com>
2024-05-12 03:16:11 +0300

    Added url button to add the bot to groups directly "missing default admin rights"

--- src/Handlers/Groups/addGroupCallback.py
+++ src/Handlers/Groups/addGroupCallback.py
@@ -4,7 +4,8 @@ from telebot import types
 def add_group_callback(call, bot):
     # Add a "Back" button
     keyboard = types.InlineKeyboardMarkup(row_width=1)
+    add_button = types.InlineKeyboardButton("Add Me To Group", url="http://t.me/Doptica_Bot?startgroup=botstart")
     back_button = types.InlineKeyboardButton("Back 🔙", callback_data='back_to_groups_menu')
-    keyboard.add(back_button)
+    keyboard.add(add_button, back_button)
 
-    bot.edit_message_text("Go to Group and Add the Bot manually", call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='Markdown')
+    bot.edit_message_text("Press (Add Me To Group) button", call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='Markdown')
