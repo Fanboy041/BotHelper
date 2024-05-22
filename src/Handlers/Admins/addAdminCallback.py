07982c1cccd39e35d5eac22c5903cc9df077e275
bitary <ahmdbytar50@gmail.com>
2024-05-23 01:25:53 +0300

    add admins buttons

--- src/Handlers/Admins/addAdminCallback.py
+++ src/Handlers/Admins/addAdminCallback.py
@@ -1,31 +1,29 @@
+# addAdminCallback.py
 from telebot import types
-from Database.MongoDB import save_admin, get_user, delete_user
+from Database.MongoDB import save_admin, get_users, delete_user
 
 def add_admin_callback(call, bot):
-
-    # If back_to_admins_menu button pressed clear the step handler
-    if call.data == 'admins_menu':
-        bot.clear_step_handler_by_chat_id(call.message.chat.id)
-        return
-    
     # Add a "Back" button
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='admins_menu')
+    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='back_to_admins_menu')
     keyboard.add(back_button)
 
-    bot.edit_message_text(
-        "⏩ Forward a message from the user you want to add as an admin.",
-        call.message.chat.id,
-        call.message.message_id,
-        reply_markup=keyboard,
-        parse_mode='Markdown'
-        )
+    #get all users from the collection and send them as buttons to the chat
+    if len(list(get_users())) > 0:
+        users = get_users()
+        for user in users:
+            users_button = types.InlineKeyboardButton(f"{user['full_name']}" , callback_data="hhh")
+            keyboard.add(users_button)
+        bot.edit_message_text("Users:\n\n", call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
+    else:
+        # There is no users
+        bot.send_message(call.message.chat.id, "There are no users.")
+
     # Set the next state to handle the forwarded message
     bot.register_next_step_handler(call.message, process_admin_forwarded_message, bot)
 
 def process_admin_forwarded_message(message, bot):
     try:
-
         # Check if message.forward_from exists and has necessary attributes
         if message.forward_from and hasattr(message.forward_from, 'id') and hasattr(message.forward_from, 'first_name'):
             # Extract user information from the forwarded message
