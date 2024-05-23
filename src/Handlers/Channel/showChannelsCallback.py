d22738ad2f8a59ebfc8c2341b10a0a479ec05e96
Fanboy041 <163625032+Fanboy041@users.noreply.github.com>
2024-05-23 23:37:53 +0300

    Update showChannelsCallback.py after review passed

--- src/Handlers/Channel/showChannelsCallback.py
+++ src/Handlers/Channel/showChannelsCallback.py
@@ -1,31 +1,16 @@
-# showChannelCallback.py
 from telebot import types
-from Database.MongoDB import (get_channel, channel_collection)
+from Database.MongoDB import get_channels
 
 def show_channels_callback(call, bot):
 
-    channels = channel_collection.find()
-
-    if channels:
-
-     
-        # Create an empty InlineKeyboardMarkup object lately i will store every channel on single one
-
+    if len(list(get_channels())) > 0:
         keyboard = types.InlineKeyboardMarkup(row_width=1)
-
-        # Loop through channels and create buttons for every one 
-
         for channel in channels:
-            button = types.InlineKeyboardButton(f"{channel['full_name']}", callback_data=f'show_channels_callback{channel["chat_id"]}')
+            button = types.InlineKeyboardButton(f"{channel['full_name']}", callback_data=f'view_channel_{channel["chat_id"]}')
             keyboard.add(button)
 
-
-        # Add a "Back" button which will display after all the channels
-        back_button = types.InlineKeyboardButton("Back 🔙", callback_data='back_to_channels_menu')
+        back_button = types.InlineKeyboardButton("Back 🔙", callback_data='channels_menu')
         keyboard.add(back_button)
-        
-
-
         
         bot.edit_message_text(
             "Select a channel:", call.message.chat.id, call.message.message_id, reply_markup=keyboard
