6eb17f0840b57cee194ee8a6ba714edeb8e60cc6
hanialhusein <hanialhusen041@gmail.com>
2024-05-23 21:27:52 +0300

    Revert "show groups as buttons instead of text"
    This reverts commit 77854e169b08f04587c11e544e3d0fa0165a1550.

--- src/Handlers/Groups/showGroupsCallback.py
+++ src/Handlers/Groups/showGroupsCallback.py
@@ -4,17 +4,14 @@ from Database.MongoDB import get_groups
 def show_groups_callback(call, bot):
     # Add a "Back" button
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='back_to_groups_menu')
-    
+    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='groups_menu')
+    keyboard.add(back_button)
 
-    # Get all groups info from the collection and send them as a button to the chat
+    # Get all groups info from the collection and send them as a message to the chat
     if len(list(get_groups())) > 0:
         groups = get_groups()
-        for group in groups:
-            groups_button = types.InlineKeyboardButton(f"{group['full_name']}" , callback_data="hhh")
-            keyboard.add(groups_button)
-        keyboard.add(back_button)
-        bot.edit_message_text("Groups:\n\n", call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
+        group_list = "\n\n".join([f"<b>{group['full_name']}</b> (@{group['username']})" for group in groups])
+        bot.edit_message_text("Groups:\n\n" + group_list, call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
     else:
         # There is no admins
         bot.send_message(call.message.chat.id, "There are no groups.")
