660b9089fab43a5744620d5060ab8b7a0de8925a
AhmadZeina <ahmadzeina821@gmail.com>
2024-05-23 17:28:44 -0300

    change the way of viewing cahnnels

--- src/Handlers/Channel/showChannelsCallback.py
+++ src/Handlers/Channel/showChannelsCallback.py
@@ -1,16 +1,36 @@
+# showChannelCallback.py
 from telebot import types
-from Database.MongoDB import get_channels
+from Database.MongoDB import (get_channel, channel_collection)
 
 def show_channels_callback(call, bot):
 
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='channels_menu')
-    keyboard.add(back_button)
+    channels = channel_collection.find()
 
-    if len(list(get_channels())) > 0:
-        channels = get_channels()
-        channel_list = "\n\n".join([f"<b>{channel['full_name']}</b> (@{channel['username']})" for channel in channels])
-        bot.edit_message_text("Channels:\n\n" + channel_list, call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
+    if channels:
 
+     
+        # Create an empty InlineKeyboardMarkup object lately i will store every channel on single one
+
+        keyboard = types.InlineKeyboardMarkup(row_width=1)
+
+        # Loop through channels and create buttons for every one 
+
+        for channel in channels:
+            button = types.InlineKeyboardButton(f"{channel['full_name']}", callback_data=f'show_channels_callback{channel["chat_id"]}')
+            keyboard.add(button)
+
+
+        # Add a "Back" button which will display after all the channels
+        back_button = types.InlineKeyboardButton("Back 🔙", callback_data='back_to_channels_menu')
+        keyboard.add(back_button)
+        
+
+
+        
+        bot.edit_message_text(
+            "Select a channel:", call.message.chat.id, call.message.message_id, reply_markup=keyboard
+        )
+        
     else:
-        bot.send_message(call.message.chat.id, "There are no channels.")
+        bot.send_message(call.message.chat.id, "No channels found.")
+
