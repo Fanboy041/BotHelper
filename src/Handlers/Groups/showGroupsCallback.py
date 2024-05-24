8cfc584115ca9436a98e748b80aa99ec0201f428
AhmadZeina <ahmadzeina821@gmail.com>
2024-05-24 18:14:57 -0300

    After pressing "Show Groups" -> added groups will show as buttons

--- src/Handlers/Groups/showGroupsCallback.py
+++ src/Handlers/Groups/showGroupsCallback.py
@@ -2,16 +2,21 @@ from telebot import types
 from Database.MongoDB import get_groups
 
 def show_groups_callback(call, bot):
-    # Add a "Back" button
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    back_button = types.InlineKeyboardButton("Back 🔙", callback_data='groups_menu')
-    keyboard.add(back_button)
-
-    # Get all groups info from the collection and send them as a message to the chat
+    
     if len(list(get_groups())) > 0:
         groups = get_groups()
-        group_list = "\n\n".join([f"<b>{group['full_name']}</b> (@{group['username']})" for group in groups])
-        bot.edit_message_text("Groups:\n\n" + group_list, call.message.chat.id, call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
+        keyboard = types.InlineKeyboardMarkup(row_width=1)
+        for group in groups:
+            button = types.InlineKeyboardButton(f"{group['full_name']}", callback_data=f'view_group_{group["chat_id"]}')
+            keyboard.add(button)
+
+        back_button = types.InlineKeyboardButton("Back 🔙", callback_data='groups_menu')
+        keyboard.add(back_button)
+        
+        bot.edit_message_text(
+            "Select a group:", call.message.chat.id, call.message.message_id, reply_markup=keyboard
+        )
+        
     else:
-        # There is no admins
-        bot.send_message(call.message.chat.id, "There are no groups.")
+        bot.send_message(call.message.chat.id, "No groups found.")
+
