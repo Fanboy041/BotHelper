4d3b149c29969005da57bff422f282fff347da13
wal6ed <waleed6.kahale@gmail.com>
2024-05-28 02:19:14 +0200

    add welcome and goodbey message if user go in or out of the Group

--- src/Features/muteComment.py
+++ src/Features/muteComment.py
@@ -1,10 +1,20 @@
 from Database.MongoDB import delete_group
 
 def mute_comment(message, bot):
+    chatId = message.chat.id
     if message.content_type == 'new_chat_members':
         new_chat_member = message.new_chat_members[0]
-        if new_chat_member.id != bot.get_me().id:
-            bot.delete_message(message.chat.id, message.message_id)
+        userId = new_chat_member.id
+        username = new_chat_member.username
+        if userId != bot.get_me().id:
+            bot.delete_message(chatId, message.message_id)
+            bot.send_message(message.chat.id, f"Welcome [{userId}](tg://user?id={userId}) @[{username}] in our Group", parse_mode = "Markdown")
+            
     elif message.content_type == 'left_chat_member':
-        if message.left_chat_member.id != bot.get_me().id:
-            bot.delete_message(message.chat.id, message.message_id)
+        left_chat_memeber = message.left_chat_member
+        userId = left_chat_memeber.id
+        username = left_chat_memeber.username
+        if userId != bot.get_me().id:
+            bot.delete_message(chatId, message.message_id)
+            bot.send_message(message.chat.id, f"Goodbye Bitch [{userId}](tg://user?id={userId}) @[{username}]", parse_mode = "Markdown")
+            
